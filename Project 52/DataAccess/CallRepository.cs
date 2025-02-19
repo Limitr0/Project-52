7f109ea43558a4067d09ac8836dad22099e453f7
Limitr0 <daniil.zateev2015@gmail.com>
2025-02-19 22:51:22 +0200

    Create CallRepository.cs

---
+++ Project 52/DataAccess/CallRepository.cs
@@ -0,0 +1,20 @@
+namespace DataAccess;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CallRepository : ICallRepository
+{
+    private List<Call> calls = new List<Call>();
+
+    public void Save(Call call)
+    {
+        calls.Add(call);
+    }
+
+    public List<Call> GetCalls()
+    {
+        return calls.OrderByDescending(c => c.CallTime).ToList();
+    }
+}
