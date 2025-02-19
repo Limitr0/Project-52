7a549a09814e7a2c5d0e6bb5f6cb402460968d28
Limitr0 <daniil.zateev2015@gmail.com>
2025-02-19 22:51:48 +0200

    Create MessageRepository.cs

---
+++ Project 52/DataAccess/MessageRepository.cs
@@ -0,0 +1,20 @@
+namespace DataAccess;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class MessageRepository : IMessageRepository
+{
+    private List<Message> messages = new List<Message>();
+
+    public void Save(Message message)
+    {
+        messages.Add(message);
+    }
+
+    public List<Message> GetMessages()
+    {
+        return messages.OrderByDescending(m => m.ReceivedTime).ToList();
+    }
+}
