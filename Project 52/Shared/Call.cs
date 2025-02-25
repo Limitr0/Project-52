e2edf6a6ed550f9be4c3680f8da7a21b6d4fe62e
Limitr0 <daniil.zateev2015@gmail.com>
2025-02-25 13:41:29 +0200

    Update Call.cs

--- Project 52/Shared/Call.cs
+++ Project 52/Shared/Call.cs
@@ -1,8 +1,8 @@
-namespace Shared;
+namespace Shared.Models;
 
 public class Call
 {
-    public string PhoneNumber { get; set; }
+    public string PhoneNumber { get; set; } = string.Empty;
     public DateTime CallTime { get; set; }
     public bool IsIncoming { get; set; }
 }
